f4e404e5c993c7da075be610c6bc3f2991cb710b
ericball1 <ericball1229@gmail.com>
2017-04-19 01:16:35 -0400

    Add title of album

--- injection.js
+++ injection.js
@@ -1,2 +1,11 @@
-var albumTitle = document.getElementsByClassName("album-title").innerHTML;
-console.log(albumTitle);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.info == "needed") {
+      var albumTitle = document.getElementsByClassName("album-title")[0].innerHTML.trim();
+      /*chrome.runtime.sendMessage({album_title: albumTitle}, function(response) {
+        console.log(response.farewell);
+        console.log(albumTitle);
+      });*/
+      sendResponse({farewell: albumTitle});
+    }
+  });
--- popup.js
+++ popup.js
@@ -5,7 +5,22 @@ chrome.tabs.query({
   var neededURL = tabs[0].url;
   var splitURL = neededURL.split("/");
   var galleryId = splitURL[splitURL.length - 1];
-  var wordpressCode = "[gallery type=\"flickr\" user_id='128947481@N05' view='photosets' photoset_id='" + galleryId +"' tag_mode='any' sort='date-posted-desc' per_page='20' layout='mosaic' caption='none' thumb_size='n' main_size='b']"
+  var wordpressCode = "[gallery type=\"flickr\" user_id='128947481@N05' view='photosets' photoset_id='" + galleryId +"' tag_mode='any' sort='date-posted-desc' per_page='20' layout='mosaic' caption='none' thumb_size='n' main_size='b']";
   document.getElementById("ID").value = wordpressCode;
   document.getElementById("ID").select();
+  getTitle();
 });
+
+function getTitle() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {info: "needed"}, function(response) {
+      // console.log(response.farewell);
+      document.getElementById("album-title").innerHTML = response.farewell;
+    });
+  });
+  
+  
+  /*chrome.runtime.sendMessage({info: "hello"}, function(response) {
+    document.getElementById("album-title").innerHTML = response.info;
+  });*/
+}
